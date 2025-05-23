2a62d876308e66c77fdccd1b9d7bdade25170372
Yeison <yeison_fexa@hotmail.com>
2025-05-23 19:53:29 -0400

    Avance dinamismo JS

---
+++ main.js
@@ -0,0 +1,10 @@
+function incrementar() {
+  // Obtenemos el valor actual del contador
+  let valor = document.getElementById('contador').textContent;
+  // Convertimos el valor a un número entero
+  valor = parseInt(valor);
+  // Incrementamos el valor
+  valor++;
+  // Actualizamos el contenido del elemento con el nuevo valor
+  document.getElementById('contador').textContent = valor;
+}
