cf8925e75fbf9277d5d2501767c853683e8c220b
Yeison <yeison_fexa@hotmail.com>
2025-05-23 20:11:44 -0400

    Dos contadores

--- main.js
+++ main.js
@@ -8,3 +8,13 @@ function incrementar() {
   // Actualizamos el contenido del elemento con el nuevo valor
   document.getElementById('contador').textContent = valor;
 }
+function incrementar2() {
+  // Obtenemos el valor actual del contador
+  let valor = document.getElementById('contador-2').textContent;
+  // Convertimos el valor a un número entero
+  valor = parseInt(valor);
+  // Incrementamos el valor
+  valor++;
+  // Actualizamos el contenido del elemento con el nuevo valor
+  document.getElementById('contador-2').textContent = valor;
+}
